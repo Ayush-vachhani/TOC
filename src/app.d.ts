370ec3ed843147cf346022329db8c68a00929afb
Ayush-vachhani <ayupatel107@gmail.com>
2024-04-16 16:52:53 +0530

    Added sample DFA.

--- src/app.d.ts
+++ src/app.d.ts
@@ -9,5 +9,4 @@ declare global {
 		// interface Platform {}
 	}
 }
-
 export {};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,48 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import {instance} from '@viz-js/viz';
+
+    let Graph:HTMLElement;
+    let initialState: string = 'a';
+    let finalStates: string[] = ['b','d'];
+    let transitions: { from: string, to: string, label: string }[] = [{ from: 'a', to: 'b', label: '0' },{ from: 'a', to: 'c', label: '1' }];
+
+    onMount(() => {
+        renderGraph();
+    });
+
+    function renderGraph() {
+        let dotGraph = `digraph {
+        rankdir=LR;
+        node [shape=circle];
+        {rank = same; node [shape=point]; init_node} -> ${initialState};
+        ${initialState};`;
+
+        for (const transition of transitions) {
+            dotGraph += `  ${transition.from} -> ${transition.to} [label="${transition.label}"];\n`;
+        }
+
+        // Specify final states
+        for (const finalState of finalStates) {
+            dotGraph += `  ${finalState} [peripheries=2];\n`;
+        }
+
+        dotGraph += `}`; // Close the digraph
+
+        instance().then(viz => {
+            Graph.innerHTML = viz.renderSVGElement(dotGraph).outerHTML;
+        });
+        console.log(dotGraph)
+    }
+
+    function addTransition() {
+        transitions = [...transitions, { from: '', to: '', label: '' }];
+    }
+
+    function removeTransition(index: number) {
+        transitions.splice(index, 1);
+    }
+
+</script>
+
+<div class="sample" bind:this={Graph}/>
